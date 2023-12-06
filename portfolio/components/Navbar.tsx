2d15984b5e75b89e625effb760856e2cd1fd79c9
vaishali023 <vaishalichaudhary011@gmail.com>
2023-12-05 22:19:29 -0500

    Feat: Add the navigation bar

---
+++ portfolio/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import { Socials } from '@/constants'
+import Image from 'next/image'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20'>
+        <div className='flex flex-row gap-3 items-center'>
+            <div className='relative'>
+            <Image
+             src='/horseLogo.jpg'
+             alt='Logo'
+             height={40}
+             width={40}
+             className='w-full h-full object-contain rounded-full'
+             />
+             </div>
+            <h1 className='text-white text-[25px] font-semibold'>Vaishali</h1>
+        </div>
+        <div className='flex flex-row gap-5 mb-2'>
+            {Socials.map((social) => (
+                 <Image 
+                 key={social.name}
+                 src={social.src}
+                 alt={social.name}
+                 width={28}
+                 height={28}
+                 />
+            )
+               
+            )}
+        </div>
+        </div>
+  )
+}
+
+export default Navbar
