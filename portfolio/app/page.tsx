a90895c70bd2aef013ee6493e0ccc866b1b9996e
vaishali023 <vaishalichaudhary011@gmail.com>
2024-01-09 22:16:06 -0500

    Refractor: Change the title

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className='w-screen h-screen relative'>
       <div className='flex items-center w-full h-full bg-cover bg-center' style={{backgroundImage: 'url(/assets/main-bg.webp)'}}>
         <div className='pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]'>
-          <h1 className="text-[50px] font-semibold text-white">Make anything possible with 
+          <h1 className="text-[50px] font-semibold text-white">Web Design &
           <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'>
             {" "}
               Web Development
