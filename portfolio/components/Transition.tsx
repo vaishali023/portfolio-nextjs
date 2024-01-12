1ab5b10c0d775af2e1240d8b1e099028793b19e0
vaishali023 <vaishalichaudhary011@gmail.com>
2024-01-11 22:30:46 -0500

    Refractor: Update Transitions

--- portfolio/components/Transition.tsx
+++ portfolio/components/Transition.tsx
@@ -1,9 +1,35 @@
 "use client"
 import React from 'react'
+import {motion } from 'framer-motion'
+
+const TransitionVariants = {
+    initial: {
+        y: "100%",
+        height: '100%'
+    },
+    animate: {
+        y: '0%',
+        height: '0%'
+    },
+    exit: {
+        y: ["0%", "100%"],
+        height: ["0%", "100%"]
+    }
+}
 
 const Transition = () => {
   return (
-    <div>Transition</div>
+    <div>
+        <motion.div 
+        className='fixed right-0 h-screen w-screen bottom-full z-[30] bg-[#2e2257]'
+        variants={TransitionVariants}
+        initial="initial"
+        exit="exit"
+        animate="animate"
+        >
+
+        </motion.div>
+    </div>
   )
 }
 
