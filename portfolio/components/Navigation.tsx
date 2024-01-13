72e52ef1b6ac50e4cdc429fcf816e736c2551aa0
vaishali023 <vaishalichaudhary011@gmail.com>
2024-01-12 22:21:53 -0500

    Refractor: Update Transition

--- portfolio/components/Navigation.tsx
+++ portfolio/components/Navigation.tsx
@@ -3,6 +3,7 @@ import { NavLinks } from '@/constants';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
+import Transition from './Transition';
 
 const Navigation = () => {
     const [isRouting, setIsRouting] = useState(false);
@@ -27,8 +28,9 @@ const Navigation = () => {
 
   return (
     <div 
-    style={{left: "20%"}}
+    style={{left: "10%"}}
      className='absolute z-[50] -bottom-20 w-[50%] md:w-[20%] max-h-[150px] rounded-full flex justify-between items-center border border-white py-7'>
+        {isRouting && <Transition/>}
         {NavLinks.map((nav) => (
             <Link key={nav.name}
             href={nav.link}
