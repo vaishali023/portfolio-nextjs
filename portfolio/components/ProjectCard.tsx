20f511197c6d1d0f079c831d9d6c8bf296fe4b17
vaishali023 <vaishalichaudhary011@gmail.com>
2024-01-23 20:26:54 -0500

    Refractor: Update project card and add it to the projects page

--- portfolio/components/ProjectCard.tsx
+++ portfolio/components/ProjectCard.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState } from 'react'
 import {motion } from 'framer-motion'
 
@@ -21,8 +22,7 @@ const ProjectCard = ({image, title, text}: Props) => {
   return (
     <div
     onClick={handleFlip}
-    className='w-[450px] h-[280px] rounded-md cursor-pointer'
-    >
+    className='w-[450px] h-[280px] rounded-md cursor-pointer'>
         <motion.div
         className='flip-card-inner w-full h-full '
         initial={false}
@@ -31,9 +31,27 @@ const ProjectCard = ({image, title, text}: Props) => {
         onAnimationComplete={() => setIsAnimating(false)}
         >
             <div 
-            style={{backgroundImage: 'url'}}
+            style={{backgroundImage: `url(${image}`}}
             
-            className='w-full h-full flip-card-front bg-cover bg-center text-white rounded-lg p-4'></div>
+            className='w-full h-full group relative flip-card-front bg-cover bg-center text-white rounded-lg p-4'>
+                <div className='absolute inset-0 w-full h-full rounded-md bg-black opacity-0 group-hover:opacity-40'/>
+                 <div className='absolute inset-0 w-full h-full text-[20px] pb-10 hidden group-hover:flex items-center z-[20] justify-center'>
+                    Learn more &gt;
+                </div>
+            </div>
+
+            <div 
+            style={{backgroundImage: `url(${image}`}}
+            
+            className='w-full h-full group relative flip-card-back bg-cover bg-center text-white rounded-lg p-4'>
+                <div className='absolute inset-0 w-full h-full rounded-md bg-black opacity-0 group-hover:opacity-50 z-[-1]'/>
+                <div className='flex flex-col gap-20 py-3 z-[30]'>
+                    <h1 className='text-white text-2xl font-semibold'>{title}</h1>
+                    <p className='text-gray-200 text-[20px]'>
+                        {text}
+                    </p>
+                </div>
+            </div>
 
         </motion.div>
     </div>
