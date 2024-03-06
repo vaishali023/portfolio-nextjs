82a652e2f692edae56e82848b10f0c8fc6171376
vaishali023 <vaishalichaudhary011@gmail.com>
2024-03-05 22:42:59 -0500

    Refractor: Update the path

--- portfolio/app/my-projects/page.tsx
+++ portfolio/app/my-projects/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div style={{backgroundImage: "url(/assets/mountains.jpg)"}}
+    <div style={{backgroundImage: "url(/assets/Mountains.jpg)"}}
     className='w-screen h-screen flex items-center justify-center bg-center bg-cover' 
     >
         <div className='grid grid-cols-2 gap-8 max-w-[90%] max-h-[90%]'>
