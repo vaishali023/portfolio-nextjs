db96034fd8199c7b410ccf8ada83dda4b62276c7
vaishali023 <vaishalichaudhary011@gmail.com>
2024-03-31 22:37:20 -0400

    Refractor: Update the form styles

--- portfolio/components/ContactForm.tsx
+++ portfolio/components/ContactForm.tsx
@@ -20,50 +20,46 @@ const ContactForm = () => {
     }
   };
   
-
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="text-center mt-80">
-        {isSubmitted ? (
-          <>
-            <h2 className='text-2xl font-bold mb-6 text-white'>Thank you for reaching out!</h2>
-            <p className="text-lg text-gray-200">I appreciate your message and will get back to you as soon as possible.</p>
-          </>
-        ) : (
-          <form onSubmit={handleSubmit} className="max-w-md mx-auto">
-            <h2 className='text-2xl font-bold mb-6 text-white'>Contact me</h2>
-            <div className='mb-4'>
-              <input
-                type='text'
-                placeholder='Your name'
-                name='name'
-                className='w-full px-3 py-2 text-sm text-gray-700 placeholder-gray-400 bg-white border-0 rounded shadow'
-              />
-            </div>
-            <div className='mb-4'>
-              <input
-                type='email'
-                placeholder='Email'
-                name='senderEmail'
-                required
-                className='w-full px-3 py-2 text-sm text-gray-700 placeholder-gray-400 bg-grey border-0 rounded shadow'
-              />
-            </div>
-            <div className='mb-4'>
-              <textarea
-                placeholder='Your message'
-                name='message'
-                maxLength={500}
-                className='w-full px-3 py-2 text-sm text-gray-700 placeholder-gray-400 bg-white border-0 rounded shadow'
-              />
-            </div>
-            <button type="submit" className='px-6 py-3 mb-5 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear bg-blue-500 hover:bg-blue-400 rounded'>Send a message</button>
-          </form>
-        )}
-      </div>
+    <div className="max-w-md mx-auto">
+      {isSubmitted ? (
+        <div className="text-center">
+          <h2 className='text-2xl font-bold mb-6 text-white'>Thank you for reaching out!</h2>
+          <p className="text-lg text-gray-200">I appreciate your message and will get back to you as soon as possible.</p>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <h2 className='text-2xl font-bold mb-6 text-white'>Contact me</h2>
+          <div className='mb-4'>
+            <input
+              type='text'
+              placeholder='Your name'
+              name='name'
+              className='w-full px-3 py-2 text-sm text-gray-700 placeholder-gray-400 bg-white border-0 rounded shadow'
+            />
+          </div>
+          <div className='mb-4'>
+            <input
+              type='email'
+              placeholder='Email'
+              name='senderEmail'
+              required
+              className='w-full px-3 py-2 text-sm text-gray-700 placeholder-gray-400 bg-grey border-0 rounded shadow'
+            />
+          </div>
+          <div className='mb-4'>
+            <textarea
+              placeholder='Your message'
+              name='message'
+              maxLength={500}
+              className='w-full px-3 py-2 text-sm text-gray-700 placeholder-gray-400 bg-white border-0 rounded shadow'
+            />
+          </div>
+          <button type="submit" className='px-6 py-3 mb-5 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear bg-blue-500 hover:bg-blue-400 rounded'>Send a message</button>
+        </form>
+      )}
     </div>
   );
 };
-
 
 export default ContactForm
